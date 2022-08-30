c9c2872d0b79690a8024a1255a722d60ac73ae52
Vinicius Ruffo Viviani - 821156267 <821156267@ulife.com.br>
2022-08-29 21:31:35 -0300

    Adicionado request

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 const axios = require('axios')
 const appid = process.env.appid
 
-const q = 'Itu'
+const q = 'Sao Paulo'
 //metric: celsius
 //imperil: ferenheint
 const units = 'metric'
@@ -13,6 +13,11 @@ const cnt = 10
 
 const base_url = 'https://api.openweathermap.org/data/2.5/forecast'
 
-const url = `${base_url}?q=${q}&${units}&lang=${lang}&cnt=${cnt}&appid=${appid}`
+const url = `${base_url}?q=${q}&units=${units}&lang=${lang}&cnt=${cnt}&appid=${appid}`
 
-console.log(url)
+const p = axios.get(url)
+p
+    .then((res) => {
+        console.log(res.data)
+        return res.data
+    })
